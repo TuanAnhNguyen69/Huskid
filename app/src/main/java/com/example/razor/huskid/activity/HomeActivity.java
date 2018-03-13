10b12fdb6984bb2c80dd60ff14130d4223eee9bb
NguyenTuanAnh <razordmonkey@live.com>
2018-03-13 11:51:55 +0700

    init

---
+++ app/src/main/java/com/example/razor/huskid/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.razor.huskid.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.razor.huskid.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
+
---
+++ app/src/main/java/com/example/razor/huskid/activity/LearnActivity.java
@@ -0,0 +1,15 @@
+package com.example.razor.huskid.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.razor.huskid.R;
+
+public class LearnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn);
+    }
+}
