dab144e035c2c6a7f11c02d1fa8c2e17463e3448
NguyenTuanAnh <razordmonkey@live.com>
2018-03-20 01:39:34 +0700

    Add crossword generate, crossword fragment

--- app/src/main/java/com/example/razor/huskid/activity/HomeActivity.java
+++ app/src/main/java/com/example/razor/huskid/activity/HomeActivity.java
@@ -2,8 +2,19 @@ package com.example.razor.huskid.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.razor.huskid.R;
+import com.example.razor.huskid.entity.CrossWord;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,6 +22,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
 
