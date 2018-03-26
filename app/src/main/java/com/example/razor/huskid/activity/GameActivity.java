2d6f38e031777f5f3051f4d1dbdced20d7c20c2c
Thức Phạm <thucp228@gmail.com>
2018-03-27 00:11:33 +0700

    update homescreen

--- app/src/main/java/com/example/razor/huskid/activity/GameActivity.java
+++ app/src/main/java/com/example/razor/huskid/activity/GameActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 
 import com.example.razor.huskid.R;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
--- app/src/main/java/com/example/razor/huskid/activity/HomeActivity.java
+++ app/src/main/java/com/example/razor/huskid/activity/HomeActivity.java
@@ -2,15 +2,23 @@ package com.example.razor.huskid.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.razor.huskid.R;
 
-public class HomeActivity extends AppCompatActivity {
-
+public class HomeActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
 
