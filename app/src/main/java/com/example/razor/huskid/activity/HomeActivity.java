26c00cc11324b80c81ba6f30a01a28cc00a85c44
Thức Phạm <thucp228@gmail.com>
2018-04-03 11:54:38 +0700

    Fix conflict

--- app/src/main/java/com/example/razor/huskid/activity/HomeActivity.java
+++ app/src/main/java/com/example/razor/huskid/activity/HomeActivity.java
@@ -2,23 +2,9 @@ package com.example.razor.huskid.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.Menu;
-=======
-import android.util.Log;
->>>>>>> c2951b70ecea366f0db0831abb5dde2b34e37693
 
 import com.example.razor.huskid.R;
-import com.example.razor.huskid.entity.CrossWord;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.Random;
 
 public class HomeActivity extends AppCompatActivity
 {
@@ -27,7 +13,6 @@ public class HomeActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
